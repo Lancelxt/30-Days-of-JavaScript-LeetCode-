1c6f3c49636c066f0beab62b42a575bc2a19ec98
Malyaj Singh <malyajsingh2001@gmail.com>
2023-11-15 13:52:03 +0530

    completed the code

---
+++ Hello World.js
@@ -0,0 +1,17 @@
+// Create a Hello World Function
+// Write a function createHelloWorld. It should return a new function that always returns "Hello World".
+
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World"
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
