2f04e544880352b112a3194b60facecc53863106
Malyaj Singh <malyajsingh2001@gmail.com>
2023-11-28 11:45:42 +0530

    modified the code

--- Timeout Cancellation.js
+++ Timeout Cancellation.js
@@ -24,33 +24,7 @@ var cancellable = function(fn, args, t) {
     }, t);
   
     return cancelFn;
-  };
-  
-  const result = [];
-  const fn = (x) => x * 5;
-  const args = [2];
-  const t = 20;
-  const cancelT = 50;
-  
-  const start = performance.now();
-  
-  const log = (...argsArr) => {
-    const diff = Math.floor(performance.now() - start);
-    result.push({ "time": diff, "returned": fn(...argsArr) });
-  };
-  
-  const cancel = cancellable(log, args, t);
-  
-  const maxT = Math.max(t, cancelT);
-  
-  setTimeout(() => {
-    cancel();
-  }, cancelT);
-  
-  setTimeout(() => {
-    console.log(result); // [{"time":20,"returned":10}]
-  }, maxT + 15);
-  
+  };  
   /**
    *  const result = []
    *
