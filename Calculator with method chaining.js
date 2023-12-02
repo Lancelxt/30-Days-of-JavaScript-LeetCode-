4ca3b700a4ba56bd59b8280e4a37e82b1837f16d
Malyaj Singh <malyajsingh2001@gmail.com>
2023-12-02 12:50:34 +0530

    initial commit

---
+++ Calculator with method chaining.js
@@ -0,0 +1,85 @@
+// Calculator
+
+// Design a Calculator class. The class should provide the mathematical operations of addition, subtraction, multiplication, division, and exponentiation. 
+// It should also allow consecutive operations to be performed using method chaining. 
+// The Calculator class constructor should accept a number which serves as the initial value of result.
+
+
+
+
+class Calculator {
+    /** 
+     * @param {number} value
+     */
+    constructor(value) {
+      this.result = value;
+    }
+  
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    add(value) {
+      this.result += value;
+      return this;
+    }
+  
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    subtract(value) {
+      this.result -= value;
+      return this;
+    }
+  
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */  
+    multiply(value) {
+      this.result *= value;
+      return this;
+    }
+  
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    divide(value) {
+      if (value === 0) {
+        throw new Error("Division by zero is not allowed");
+      }
+      this.result /= value;
+      return this;
+    }
+  
+    /** 
+     * @param {number} value
+     * @return {Calculator}
+     */
+    power(value) {
+      this.result **= value;
+      return this;
+    }
+  
+    /** 
+     * @return {number}
+     */
+    getResult() {
+      return this.result;
+    }
+  }
+  
+  // Example usage:
+  const myCalculator = new Calculator(10);
+  const result = myCalculator
+    .add(5)
+    .subtract(3)
+    .multiply(2)
+    .divide(4)
+    .power(2)
+    .getResult();
+  
+  console.log(result); // Output: 18.75
+  
